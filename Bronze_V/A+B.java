eef25880eeec6d072c279029258ae82d60831c7c
EasternBlaze <97613425+EasternBlaze@users.noreply.github.com>
2022-01-15 11:05:56 +0900

    Create A+B.java

---
+++ Bronze_V/A+B.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);    
+        int a;
+        a = scan.nextInt();
+        int b;
+        b = scan.nextInt();
+        System.out.println(a+b);
+    }
+}
