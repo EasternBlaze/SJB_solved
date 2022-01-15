f5b48f40f15a27c909250e3ed71a4c17b442e5ca
EasternBlaze <97613425+EasternBlaze@users.noreply.github.com>
2022-01-15 11:08:50 +0900

    Update A+B.java
    Reflected new condition (0<A,B<10)

--- Bronze_V/A+B.java
+++ Bronze_V/A+B.java
@@ -1,11 +1,12 @@
-import java.util.*;
+import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);    
-        int a;
-        a = scan.nextInt();
-        int b;
-        b = scan.nextInt();
-        System.out.println(a+b);
+        Scanner scan = new Scanner(System.in);
+         int A, B; 
+         A = scan.nextInt();
+         B = scan.nextInt();
+        if(0<A && A < 10 && 0<B && B<10){
+            System.out.println(A + B);
+        }
     }
 }
