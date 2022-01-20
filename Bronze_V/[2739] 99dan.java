384d9dad98fc6ab1d6fbdfeded303808899aaaa6
EasternBlaze <easternblaze0702@gmail.com>
2022-01-20 12:39:32 +0900

    Create [2739] 99dan.java
    also could be done with 2 for loops

---
+++ Bronze_V/[2739] 99dan.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+    for(int counter = 1; counter <= 9; counter++){
+      
+      System.out.println(n + " * " + counter + " = " + counter*n);
+    }
+  }
+}
